d06a8dec25e7220c2f5a99f0b8247401ea0b3cb0
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-13 09:39:38 -0300

    initial

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    client: 'pg',
+    connection: {
+      database: 'db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  
+}
