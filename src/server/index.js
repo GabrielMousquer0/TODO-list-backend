8d90a0b75cdc38745625e35208b5646b297263bd
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-16 14:01:07 -0300

    🔐 passando a variavel de ambiente para um arquivo .env

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
 const { ApolloServer } = require("apollo-server")
 const { resolvers, typeDefs } = require("../graphql/schemas.js")
+require('dotenv').config()
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
 
-server.listen({port: 8080}).then(({url}) => console.log(url))
+server.listen({port: process.env.SERVER_PORT}).then(({url}) => console.log(url))
 
